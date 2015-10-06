8e1f70577857a70e9cffd47a874f5ea907612add
aldonk <459377460@qq.com>
2015-10-06 23:26:52 +0800

    新增数据库帮助类，及各表对应实体类

---
+++ app/src/main/java/com/sxc/cai/weather/model/Province.java
@@ -0,0 +1,34 @@
+package com.sxc.cai.weather.model;
+
+/**
+ * Created by cai on 2015/10/6.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
